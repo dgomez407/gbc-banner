ac40efb990b07310294f890f5ea183f2b7647a6c
Joshua Bittel <joshuabittel05@gmail.com>
2022-02-20 22:08:30 -0500

    Added scrolling image with auto resize div

--- assets/js/scriptBanner.js
+++ assets/js/scriptBanner.js
@@ -1,21 +1,20 @@
 
 // Parameters
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-seconds = urlParams.get('i')
-
-// Rotation
-window.onload = function() { console.log('hi'); rotate(seconds); };
+//const queryString = window.location.search;
+//const urlParams = new URLSearchParams(queryString);
+//seconds = urlParams.get('i')
+const seconds = 1
 var theImage = 0;
 var images = new Array("images/banner/banner01.jpg", "images/banner/banner02.jpg", "images/banner/banner03.jpg");
+
+window.onload = function() { console.log('hi'); rotate(seconds); };
 function rotate(seconds) {
-    console.log('hi');
-    // if (seconds == null) { seconds = 1; }
-    // seconds = 1;
-    // theImage++;
-    // if (theImage == images.length) {
-    //     theImage = 0;
-    // }
-    // document.getElementById("adBanner").src = images[theImage];
-    // setTimeout(rotate, seconds * 1000);
+    if (seconds == null) { seconds = 1; }
+     if (theImage == images.length) {
+         theImage = 0;
+     }
+     //document.getElementById("banner").style.backgroundImage = "url(" + images[theImage] + ")";
+	 document.getElementById("bannerAd").src = images[theImage];
+	 theImage++;
+     setTimeout(rotate, seconds * 1000);
 }
