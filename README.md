3160baea0fa6edbdac623a1835fcb25656cd5421
Daniel Gomez <dgomez407@gmail.com>
2022-02-26 13:28:09 -0500

    added time interval parameter and refactored

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Javascript code to rotate through images and present on a webpage.
 
-## Production
-The main URL operating this code is located at https://greenbeltbaptist.org/banner.html
-
 ## Notes
 The solution does not work on chrome but does on edge and smart TV browsers.
+
+Links
+* The main URL is [https://greenbeltbaptist.org/banner.html](https://greenbeltbaptist.org/banner.html)
+* The test URL is [https://greenbeltbaptist.org/banner1.html](https://greenbeltbaptist.org/banner1.html)
+
+The parameter "i" can be passed to set the time interval in seconds. The default time interval is 20 seconds. The following example sets the interval to 2 seconds. See [https://greenbeltbaptist.org/banner.html?i=2](https://greenbeltbaptist.org/banner.html?i=2).
--- assets/js/scriptBanner.js
+++ assets/js/scriptBanner.js
@@ -1,20 +1,40 @@
 
 // Parameters
-//const queryString = window.location.search;
-//const urlParams = new URLSearchParams(queryString);
-//seconds = urlParams.get('i')
-const seconds = 1
-var theImage = 0;
-var images = new Array("images/banner/banner01.jpg", "images/banner/banner02.jpg", "images/banner/banner03.jpg");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const urlParams_i = urlParams.get('i')
+const secondsDefault = 20
+const theImageIndexDefault = 0
 
-window.onload = function() { console.log('hi'); rotate(seconds); };
+// Variables
+var seconds = urlParams_i
+var theImage = theImageIndexDefault;
+var images = new Array(
+    "images/banner/banner01.jpg", 
+    "images/banner/banner02.jpg", 
+    "images/banner/banner03.jpg",
+    "images/banner/banner04.jpg",
+    "images/banner/banner05.jpg",
+    "images/banner/banner06.jpg",
+    "images/banner/banner07.jpg",
+    "images/banner/banner08.jpg"
+    );
+
+// Functions
 function rotate(seconds) {
-    if (seconds == null) { seconds = 1; }
-     if (theImage == images.length) {
-         theImage = 0;
-     }
-     //document.getElementById("banner").style.backgroundImage = "url(" + images[theImage] + ")";
-	 document.getElementById("bannerAd").src = images[theImage];
-	 theImage++;
-     setTimeout(rotate, seconds * 1000);
+    //console.log('theImage=' + theImage);
+    theImage++;
+    if (theImage == images.length) { 
+        theImage = theImageIndexDefault; 
+    }
+    document.getElementById("bannerAd").src = images[theImage];
 }
+
+// Main
+if (seconds == null) { 
+    seconds = secondsDefault; 
+}
+window.onload = function() { 
+    //console.log('Start "function()"'); 
+    setInterval(rotate, seconds * 1000);
+};
